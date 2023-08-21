200fe31f52945596e09594892ef4f18152b5f617
Antonio Jorge <141699267+AntonioJFJ@users.noreply.github.com>
2023-08-21 20:26:42 -0300

    Add files via upload

--- ex033.py
+++ ex033.py
@@ -15,3 +15,32 @@ elif (num2-num1) <=0 and (num2-num3) <=0:
     print('O menor número é {}.'.format(num2))
 elif (num3-num1) <=0 and (num3-num2) <=0:
     print('O menor número é {}.'.format(num3))
+
+
+
+
+'''Código mais compacto:
+1)
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+num3 = int(input('Digite o terceiro número: '))
+ordem = [num1, num2, num3]
+ordem.sort()
+print(f'O menor número é {ordem[0]} e o maior é {ordem[2]}')
+--------------------------------------------------------------
+2)
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+num3 = int(input('Digite o terceiro número: '))
+menor = num1
+if num2<num1 and num2<num3:
+    menor = num2
+if num3<num1 and num3<num2:
+    menor = num3
+print('O menor número é {}'.format(menor))
+maior = num1
+if num2>num1 and num2>num3:
+    maior = num2
+if num3>num1 and num3>num2:
+    maior = num3
+print('O maior número é {}'.format(maior))'''
