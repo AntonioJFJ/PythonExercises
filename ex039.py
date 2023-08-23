74aeb187148bf83753ca678ec82ce538c7be1e68
Antonio Jorge <141699267+AntonioJFJ@users.noreply.github.com>
2023-08-23 18:06:37 -0300

    Add files via upload

--- ex039.py
+++ ex039.py
@@ -10,16 +10,22 @@ time.sleep(1)
 nome = str(input('Qual é o seu nome?\n'))
 time.sleep(1)
 print('Seja bem-vindo às Forças Armadas, \033[32m{}\033[m!'.format(nome))
-ano = int(input('Informe seu ano de nascimento: '))
+sexo = int(input('Informe o sexo:\n[1]: Masculino\n[2]: Feminino\n'))
+
+
+if sexo == 2:
+    print('Alistamento obrigatório apenas para o sexo Masculino.')
+    exit()
+elif sexo == 1:
+    ano = int(input('Informe seu ano de nascimento: '))
+
 idade = date.today().year - ano
 
 if idade < 18:
     print('\033[32mIdade mínima para alistamento NÃO atingida!\nVocê deverá se apresentar em {} ano(s).'
           .format((ano+18) - date.today().year))
-
 elif idade == 18:
-    print('\033[1:33mApresente-se às Forças Armadas para apresentação e devido alistamento!')
-
+    print('\033[1:33mApresente-se às Forças Armadas para devido alistamento!')
 else:
     reservista = int(input('Você é reservista/dispensado?\n[1]: Sim\n[2]: Não\n'))
 if idade > 18 and reservista == 1:
