26dfc44d05d50d1b5af12e760675df94c39be8ac
Antonio Jorge <141699267+AntonioJFJ@users.noreply.github.com>
2023-08-24 21:14:22 -0300

    Add files via upload

--- ex050.py
+++ ex050.py
@@ -1,9 +1,15 @@
 # Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares.
 # Se o valor digitado for ímpar, desconsidere-o.
 
+cont = 0
 soma = 0
 for c in range(1, 7):
     números = int(input('Digite o {}º número: '.format(c)))
     if (números%2) == 0:
+        cont = cont + 1
         soma = soma + números
-print('\033[33mA soma dos números pares é {}.\033[m'.format(soma))
+print('\033[33m{} número(s) par(es) e sua soma é igual a {}.\033[m'.format(cont, soma))
+
+
+#soma += números
+#cont += 1
