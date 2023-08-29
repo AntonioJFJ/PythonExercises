f541f955cfe32cadb7c6324a7e6c0921af2dbb42
Antonio Jorge <141699267+AntonioJFJ@users.noreply.github.com>
2023-08-29 01:36:25 -0300

    Add files via upload

---
+++ ex057.py
@@ -0,0 +1,11 @@
+# Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores ‘M’ ou ‘F’.
+# Caso esteja errado, peça a digitação novamente até ter um valor correto.
+
+sexo = ''
+
+while sexo != 'M' and sexo != 'F':
+    sexo = str(input('Informe seu sexo:\nM | F\n')).upper()
+    if sexo != 'M' and sexo != 'F':
+        print('\033[31mResposta inválida.\033[m\n')
+
+print('Obrigado.')
