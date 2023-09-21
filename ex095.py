50fcbdff5255a10e35cd75030f1a5349cf4c02b5
Antonio Jorge <141699267+AntonioJFJ@users.noreply.github.com>
2023-09-20 21:43:38 -0300

    Add files via upload

--- ex095.py
+++ ex095.py
@@ -6,6 +6,8 @@ lista = []
 partidas = []
 
 while True:
+    partidas.clear()
+    jogador.clear()
     jogador['Nome'] = str(input('Nome: '))
     total = int(input(f'Nº de partidas de {jogador["Nome"]}: '))
     if total != 0:
@@ -14,8 +16,6 @@ while True:
     jogador['Gols'] = partidas[:]
     jogador['Total de gols'] = sum(jogador['Gols'])
     lista.append(jogador.copy())
-    partidas.clear()
-    jogador.clear()
     pergunta = str(input(' -> \033[7mQuer continuar [S/N]?\033[m ')).lower().strip()[0]
     while pergunta not in 'sn':
         pergunta = str(input(' -> \033[7mQuer continuar [S/N]?\033[m ')).lower().strip()[0]
@@ -25,21 +25,41 @@ while True:
         break
 
 print('-='*30)
-print(f'\033[35m{"Nº":<5}{"Nome":^10}{"Gols":^15}{"Total":>5}\033[m')
-print('-'*40)
-for índice in range(len(lista)):
-    print(f'\033[35m{índice}     {lista[índice]["Nome"]}     {lista[índice]["Gols"]}     {lista[índice]["Total de gols"]}\033[m')
+print('\033[35mNº \033[m', end='')
+for cabeçalho in jogador.keys():
+    print(f'\033[35m{cabeçalho:<15}\033[m', end='')
+print()
+print('-'*50)
+for k, v in enumerate(lista):
+    print(f'\033[35m{k:>2} \033[m', end='')
+    for d in v.values():
+        print(f'\033[35m{str(d):<15}\033[m', end='')
+    print()
 print('-='*30)
 
 opção = int(input('Digite o Nº para ver os dados do jogador (999 para sair): '))
 while opção != 999:
     if 0 <= opção <= len(lista)-1:
         print(f' -> \033[7mDados do jogador {lista[opção]["Nome"]}\033[m')
-        for i in range(len(lista[opção]["Gols"])):
-            print(f'Na partida {i+1} fez {lista[opção]["Gols"][i]} gol(s).')
+        for j, g in enumerate(lista[opção]["Gols"]):
+            print(f'Na partida {j+1} fez {g} gol(s).')
     else:
         print('\033[31mOpção inválida. Verifique o Nº e tente novamente.\033[m')
     opção = int(input('Digite o Nº para ver os dados do jogador (999 para sair): '))
 
 print()
 print('\033[7:33m<<Obrigado>>\033[m')
+
+
+
+'''
+1)
+print(f'\033[35m{"Nº":<5}{"Nome":^10}{"Gols":^15}{"Total":>5}\033[m')
+print('-'*40)
+for índice in range(len(lista)):
+    print(f'\033[35m{índice}     {lista[índice]["Nome"]}     {lista[índice]["Gols"]}     {lista[índice]["Total de gols"]}\033[m')
+    
+2)
+for i in range(len(lista[opção]["Gols"])):
+    print(f'Na partida {i+1} fez {lista[opção]["Gols"][i]} gol(s).')
+'''
